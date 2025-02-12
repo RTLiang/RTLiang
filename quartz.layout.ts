a8dfa8f37d1c9e4daa194287969a338e47be1c1b
RTLiang <54389220+RTLiang@users.noreply.github.com>
2025-02-12 17:11:41 +0800

    Quartz sync: Feb 12, 2025, 5:11 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -20,6 +20,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
+    Component.MobileOnly(Component.TableOfContents()),
   ],
   left: [
     Component.PageTitle(),
